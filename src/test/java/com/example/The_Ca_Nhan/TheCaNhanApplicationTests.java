081e7a9864fd253f95b17bf1f27c10a0ca2a0b1a
Nguyen Chi Tam <tamn0443@gmail.com>
2025-06-02 23:39:09 +0700

    init project

---
+++ src/test/java/com/example/The_Ca_Nhan/TheCaNhanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.The_Ca_Nhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCaNhanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
